b03a01478adf0bca1b1c37445216dcc0dcd5d6c9
guadavaquer <guadavaquer@gmail.com>
2024-06-17 13:05:30 -0300

    Configuracion spring initializer

---
+++ src/main/java/ar/edu/davinci/dv_ds_20241c_g16/DvDs20241cG16Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dv_ds_20241c_g16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20241cG16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20241cG16Application.class, args);
+	}
+
+}
